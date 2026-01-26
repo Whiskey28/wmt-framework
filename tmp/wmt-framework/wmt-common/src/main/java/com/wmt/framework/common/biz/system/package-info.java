ebbccf915405974991e37abaa09c435d229db8ed
wangmt <w1041859017@163.com>
2026-01-26 16:45:46 +0800

    feature:fada抖音数据分析v2;refacrot:delete-tmp

--- tmp/wmt-framework/wmt-common/src/main/java/com/wmt/framework/common/biz/system/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 针对 system 模块的 api 包
- */
-package com.wmt.framework.common.biz.system;
--- tmp/wmt-framework/wmt-spring-boot-starter-biz-data-permission/src/main/java/com/wmt/framework/datapermission/core/rule/dept/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 基于部门的数据权限规则
- *
- * @author wmt
- */
-package com.wmt.framework.datapermission.core.rule.dept;
